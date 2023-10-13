c0d6fef16593dd02dc6b76d31cacf8952bf4127a
Don Kieu <2177651@talnet.nl>
2023-10-13 14:33:11 +0200

    form post

--- inlog/registratie.php
+++ inlog/registratie.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
     <head>
     <title>connectie</title>
@@ -8,22 +7,17 @@
     <?php
 
     include "../Database/database.php";
-    $Naam =  ($_POST["name"]);
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$Naam)) {
-      $nameErr = "Only letters and white space allowed";
-    }
 
-    $Email = ($_POST["email"]);
-    if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "ongeldige email adres";
-    }
-    $Wachtwoord = $_POST['wachtwoord'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $naam = $_POST['naam'];
+      $email = $_POST['email'];
+      $wachtwoord = $_POST['wachtwoord'];
+      $conn->query(
+          "INSERT INTO `klanten`(`naam`, `email`, `wachtwoord`) 
+                                      VALUES ('$naam', '$email', $wachtwoord);"
+      );
 
-    if (isset($_GET['email']) && isset($_GET['Naam'])) {
-        $Email = htmlspecialchars($_GET['email']);
     }
-    $sql = "INSERT INTO klanten VALUES 
-        ('$Naam', '$Email', '$Wachtwoord')";
 
     header("Location:../hoofdpagina/hoofdpagina.html");
     ?>
